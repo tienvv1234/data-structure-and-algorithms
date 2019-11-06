d12c1edb1fef86b0f5e475f1048517545b57e0c2
tienvv1234 <tienvv1234@gmail.com>
2019-11-07 00:31:21 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+good code can describe in two things:
+
+1. Readable
+2. Scalable
+
+Big-O notation is what allow us to measure this idea of scalable code that can scale
+
+we can measure the big-O of function using the performance.now()
+
+1. O(n) Linear time
+2. O(1) Constant time
+
+## lecture Big O
+
+### section 5 O(n)
---
+++ findnemo.js
@@ -0,0 +1,18 @@
+const { performance } = require('perf_hooks');
+const nemo = ['nemo'];
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'gill', 'bloat', 'nigel', 'squirt', 'darla', 'hank']
+const large = new Array(1000).fill('nemo');
+console.log('large', large)
+function findNemo(array) {
+    let t0 = performance.now();
+    for (let index = 0; index < array.length; index++) {
+        if (array[index] === 'nemo') {
+            console.log('Found NEMO!');
+        }
+
+    }
+    let t1 = performance.now();
+    console.log('Call to find Nemo took ' + (t1 - t0) + ' milliseconds');
+}
+
+findNemo(large); 
